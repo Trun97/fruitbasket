91f53ef478d7afccfc915e516b9bc29db23b2b70
Trun97 <tarun7283@gmail.com>
2025-02-13 20:50:43 +0100

    html/css grof

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,67 @@
-
 import './App.css'
 
 function App() {
 
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+    return (
+        <>
+        <main>
+        <section className="outer-coll">
+            <h1>🍓Aardbei</h1>
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+        </section>
+        <section className="outer-coll">
+            <h1>🍌Banaan</h1>
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+        </section>
+        <section className="outer-coll">
+            <h1>🍏Appel</h1>
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+        </section>
+        <section className="outer-coll">
+            <h1>🥝Kiwi</h1>
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+        </section>
+        <button className="reset-button">Reset</button>
+        </main>
+            <footer>
+                <p>Voornaam</p>
+                <input/>
+                <p>Achternaam</p>
+                <input/>
+                <p>Leeftijd</p>
+                <input/>
+                <p>Postcode</p>
+                <input/>
+                <p>Bezorgfrequentie</p>
+                <select id="dropdown">
+                    <option value="iedere week">iedere week</option>
+                </select>
+                <br/>
+                <br/>
+                <p>Overdag</p>
+                <input type="radio"/>
+                <p>'s Avonds</p>
+                <input type="radio"/>
+                <p>Opmerking</p>
+                <textarea>
+                <input type="text"/>
+                </textarea>
+                <br/>
+                <br/>
+                <input type="checkbox"/>
+                <p>Ik ga akkoord met de voorwaarden</p>
+                <button>Verzend</button>
+            </footer>
+        </>
+    )
 }
 
 export default App
