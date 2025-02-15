21558d44da454d13ebc2b21e08b9a8a6df98c8b9
Trun97 <tarun7283@gmail.com>
2025-02-15 10:24:48 +0100

    html verbeterd, counter af

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,69 @@
 import './App.css'
+import {useState} from "react";
+import Button from "./components/Button/Button.jsx";
 
 function App() {
-
     return (
         <>
-        <main>
-        <section className="outer-coll">
-            <h1>🍓Aardbei</h1>
-            <button>+</button>
-            <span>0</span>
-            <button>-</button>
-        </section>
-        <section className="outer-coll">
-            <h1>🍌Banaan</h1>
-            <button>+</button>
-            <span>0</span>
-            <button>-</button>
-        </section>
-        <section className="outer-coll">
-            <h1>🍏Appel</h1>
-            <button>+</button>
-            <span>0</span>
-            <button>-</button>
-        </section>
-        <section className="outer-coll">
-            <h1>🥝Kiwi</h1>
-            <button>+</button>
-            <span>0</span>
-            <button>-</button>
-        </section>
-        <button className="reset-button">Reset</button>
-        </main>
+            <main>
+                <section className="outer-coll">
+                    <h1>🍓Aardbei</h1>
+                    <Button/>
+                </section>
+                <section className="outer-coll">
+                    <h1>🍌 Banaan</h1>
+                    <Button/>
+                </section>
+                <section className="outer-coll">
+                    <h1>🍏 Appel</h1>
+                    <Button/>
+                </section>
+                <section className="outer-coll">
+                    <h1>🥝 Kiwi</h1>
+                    <Button/>
+                </section>
+                <div>
+                    <button type="button" className="reset-button" onClick={()=>setSum(0)}>Reset</button>
+                </div>
+            </main>
             <footer>
-                <p>Voornaam</p>
-                <input/>
-                <p>Achternaam</p>
-                <input/>
-                <p>Leeftijd</p>
-                <input/>
-                <p>Postcode</p>
-                <input/>
-                <p>Bezorgfrequentie</p>
-                <select id="dropdown">
-                    <option value="iedere week">iedere week</option>
-                </select>
-                <br/>
-                <br/>
-                <p>Overdag</p>
-                <input type="radio"/>
-                <p>'s Avonds</p>
-                <input type="radio"/>
-                <p>Opmerking</p>
-                <textarea>
-                <input type="text"/>
-                </textarea>
-                <br/>
-                <br/>
-                <input type="checkbox"/>
-                <p>Ik ga akkoord met de voorwaarden</p>
-                <button>Verzend</button>
+                <form>
+                    <label htmlFor="first-name">Voornaam:</label>
+                    <input type="text" id="first-name"/>
+                    <br/>
+                    <label htmlFor="last-name">Achternaam:</label>
+                    <input type="text" id="last-name"/>
+                    <br/>
+                    <label htmlFor="age">Leeftijd:</label>
+                    <input type="number" id="age"/>
+                    <br/>
+                    <label htmlFor="postal-code">Postcode</label>
+                    <input type="text" id="postal-code"/>
+                    <br/>
+                    <label htmlFor="deliver-freq">Bezorgfrequentie</label>
+                    <select id="deliver-freq">
+                        <option>iedere week</option>
+                        <option>om de week</option>
+                        <option>iedere maand</option>
+                    </select>
+                    <br/>
+                    <label>
+                        <input type="radio"/>
+                        Overdag</label>
+                    <label>
+                        <input type="radio"/>
+                        s Avonds</label>
+                    <br/>
+                    <label htmlFor="remark">Opmerking:</label>
+                    <textarea id="remark" ></textarea>
+                    <br/>
+                    <label htmlFor="terms">
+                        <input type="checkbox" id="terms"/>
+                        Ik ga akkoord met de voorwaarden
+                    </label>
+                    <br/>
+                    <button type="submit">Verzend</button>
+                </form>
             </footer>
         </>
     )
