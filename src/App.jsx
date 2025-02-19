e8fc5b65cb6104ec21207942d0d73b0cbd31bee0
Trun97 <tarun7283@gmail.com>
2025-02-19 13:16:28 +0100

    components opdr 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import React, {useState} from "react";
+import Button from "./components/Button/Button.jsx";
+
 
 function App() {
 
@@ -7,10 +9,11 @@ function App() {
         aardbei: 0,
         banaan: 0,
         appel: 0,
-        kiwi: 0});
+        kiwi: 0
+    });
 
     function addButton(fruit) {
-        setSum(function(prevSum) {
+        setSum(function (prevSum) {
             let newSum = {
                 aardbei: prevSum.aardbei,
                 banaan: prevSum.banaan,
@@ -23,7 +26,7 @@ function App() {
     }
 
     function subtractButton(fruit) {
-        setSum(function(prevSum) {
+        setSum(function (prevSum) {
             let newSum = {
                 aardbei: prevSum.aardbei,
                 banaan: prevSum.banaan,
@@ -34,12 +37,14 @@ function App() {
             return newSum;
         });
     }
-    function handleReset () {
+
+    function handleReset() {
         setSum({
             aardbei: 0,
             banaan: 0,
             appel: 0,
-            kiwi: 0});
+            kiwi: 0
+        });
     }
 
 
@@ -64,7 +69,7 @@ function App() {
         });
     }
 
-    function handleSubmit (e) {
+    function handleSubmit(e) {
         e.preventDefault();
         console.log(sum);
         console.log(formState);
@@ -73,67 +78,70 @@ function App() {
     return (
         <>
             <main>
-                <section className="outer-coll">
-                    <h1>🍓Aardbei</h1>
+                <Button fruitTitle="🍓Aardbei">
                     <button type="button" onClick={() => subtractButton("aardbei")}>-</button>
                     <span>{sum.aardbei}</span>
                     <button type="button" onClick={() => addButton("aardbei")}>+</button>
-                </section>
-                <section className="outer-coll">
-                    <h1>🍌 Banaan</h1>
+                </Button>
+                <Button fruitTitle="🍌 Banaan">
                     <button type="button" onClick={() => subtractButton("banaan")}>-</button>
                     <span>{sum.banaan}</span>
                     <button type="button" onClick={() => addButton("banaan")}>+</button>
-                </section>
-                <section className="outer-coll">
-                    <h1>🍏 Appel</h1>
+                </Button>
+                <Button fruitTitle="🍏 Appel">
                     <button type="button" onClick={() => subtractButton("appel")}>-</button>
                     <span>{sum.appel}</span>
                     <button type="button" onClick={() => addButton("appel")}>+</button>
-                </section>
-                <section className="outer-coll">
-                    <h1>🥝 Kiwi</h1>
+                </Button>
+                <Button fruitTitle="🥝 Kiwi">
                     <button type="button" onClick={() => subtractButton("kiwi")}>-</button>
                     <span>{sum.kiwi}</span>
                     <button type="button" onClick={() => addButton("kiwi")}>+</button>
-                </section>
+                </Button>
                 <div>
                     <button type="button" className="reset-button" onClick={handleReset}>Reset</button>
                 </div>
             </main>
             <footer>
                 <form onSubmit={handleSubmit}>
-                <label htmlFor="first-name">Voornaam:</label>
-                    <input type="text" name= "firstName" id="first-name" value={formState.firstName} onChange={handleChange}/>
+                    <label htmlFor="first-name">Voornaam:</label>
+                    <input type="text" name="firstName" id="first-name" value={formState.firstName}
+                           onChange={handleChange}/>
                     <br/>
                     <label htmlFor="last-name">Achternaam:</label>
-                    <input type="text" name= "lastName" id="last-name" value={formState.lastName} onChange={handleChange}/>
+                    <input type="text" name="lastName" id="last-name" value={formState.lastName}
+                           onChange={handleChange}/>
                     <br/>
                     <label htmlFor="age">Leeftijd:</label>
                     <input type="number" name="age" id="age" value={formState.age} onChange={handleChange}/>
                     <br/>
                     <label htmlFor="postal-code">Postcode</label>
-                    <input type="text" name="postalCode" value={formState.postalCode} id="postal-code" onChange={handleChange}/>
+                    <input type="text" name="postalCode" value={formState.postalCode} id="postal-code"
+                           onChange={handleChange}/>
                     <br/>
                     <label htmlFor="deliver-freq">Bezorgfrequentie</label>
-                    <select id="deliver-freq" name="deliverFrequency" value={formState.deliverFrequency} onChange={handleChange}>
+                    <select id="deliver-freq" name="deliverFrequency" value={formState.deliverFrequency}
+                            onChange={handleChange}>
                         <option value={"iedere week"}>iedere week</option>
                         <option value={"om de week"}>om de week</option>
                         <option value={"iedere maand"}>iedere maand</option>
                     </select>
                     <br/>
                     <label>
-                        <input type="radio" name="timeSlot" value="Overdag" checked={formState.timeSlot === "Overdag"} onChange={handleChange} />
+                        <input type="radio" name="timeSlot" value="Overdag" checked={formState.timeSlot === "Overdag"}
+                               onChange={handleChange}/>
                         Overdag</label>
                     <label>
-                        <input type="radio" name="timeSlot" value="'s Avonds" checked={formState.timeSlot === "'s Avonds"} onChange={handleChange}/>
+                        <input type="radio" name="timeSlot" value="'s Avonds"
+                               checked={formState.timeSlot === "'s Avonds"} onChange={handleChange}/>
                         s Avonds</label>
                     <br/>
                     <label htmlFor="remark">Opmerking:</label>
-                    <textarea id="remark" name= "remark" value={formState.remark} onChange={handleChange}></textarea>
+                    <textarea id="remark" name="remark" value={formState.remark} onChange={handleChange}></textarea>
                     <br/>
                     <label htmlFor="terms">
-                        <input type="checkbox" id="terms" name="terms" checked={formState.terms} onChange={handleChange}/>
+                        <input type="checkbox" id="terms" name="terms" checked={formState.terms}
+                               onChange={handleChange}/>
                         Ik ga akkoord met de voorwaarden
                     </label>
                     <br/>
